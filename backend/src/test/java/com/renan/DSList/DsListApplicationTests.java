b16aadd79114f6a285a45543999b132e57e54445
Renan Barbosa <renan.barbosa@netbiis.local>
2023-07-10 21:24:08 -0300

    Project created

---
+++ backend/src/test/java/com/renan/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
