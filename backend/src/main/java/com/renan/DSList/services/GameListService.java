2f2d425d33c998468f7b5e4d45e13dec48e41778
Renan Barbosa <renan.barbosa@netbiis.local>
2023-07-15 10:45:55 -0300

    WebConfig e replacements

--- backend/src/main/java/com/renan/DSList/services/GameListService.java
+++ backend/src/main/java/com/renan/DSList/services/GameListService.java
@@ -2,7 +2,9 @@ package com.renan.DSList.services;
 
 import com.renan.DSList.dtos.GameListDTO;
 import com.renan.DSList.entities.GameList;
+import com.renan.DSList.projections.GameMinProjection;
 import com.renan.DSList.repositories.GameListRepository;
+import com.renan.DSList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +18,24 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
